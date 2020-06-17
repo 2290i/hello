e0888f55ce4af0d5c234fe85230affa789abee43
puttinun <puttinun@MacBook-Air-khxng-puttinun.local>
2020-06-17 18:17:00 +0700

    test go routine

--- hello.go
+++ hello.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello golang...")
+	begin := time.Now()
+	search("test")
+	end := time.Now()
+	fmt.Println(end.Sub(begin))
+}
+
+func search(keyword string) {
+	folders := [3]string{"one", "two", "three"}
+	var wg sync.WaitGroup
+	wg.Add(len(folders))
+	defer wg.Wait()
+	for _, folder := range folders {
+		go searchFromFolder(keyword, folder, &wg)
+	}
+}
+
+func searchFromFolder(keyword string, folder string, wg *sync.WaitGroup) {
+	fmt.Println("Finding in " + folder)
+	time.Sleep(1 * time.Second)
+	wg.Done()
 }
